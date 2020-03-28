c35afa375d3a310d3d0e4c24072a265bb71b6018
ZOI <zoizoichannel@gmail.com>
2020-03-28 19:48:06 +0900

    .meta del

---
+++ taiko_play.js
@@ -0,0 +1,13 @@
+// 変数宣言
+let frame = 0
+function setup() {
+    createCanvas(1280, 720)
+    textSize(40)
+    textAlign(CENTER)
+    frameRate(60)
+}
+function draw() {
+    frame++
+    for (let i = 0; i < 25; i++) {
+    }
+}
